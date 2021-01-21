c4a9555bd651bf0657b026a08927e5c2ce43cdbf
Hitchhiker1998 <xuchunebi1998@gmail.com>
2021-01-21 20:34:19 +0400

    heroku again

--- server/index.js
+++ server/index.js
@@ -20,6 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.json({hello: "hello sire"});
+})
+
 app.get("/api/user/auth", auth, (req, res) => {
   res.status(200).json({
     _id: req._id,
