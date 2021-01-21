0f41f43244f057c294cd76019fada92ff768f7e6
Hitchhiker1998 <xuchunebi1998@gmail.com>
2021-01-21 20:05:27 +0400

    register, login and logout added

---
+++ server/index.js
@@ -0,0 +1,76 @@
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const { User } = require("./models/user.js");
+const auth = require("./middleware/auth");
+console.log(auth);
+
+const config = require("./config/key");
+
+console.log("hello " + config.mongoURI, config);
+
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.errror(err));
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+app.get("/api/user/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req._id,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+  });
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
+app.post("/api/users/login", (req, res) => {
+  //find the email
+
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSucess: false,
+        message: "Auth failed, email not found",
+      });
+    }
+    //Compare the Password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ loginSucess: false, message: "Wrong password" });
+      }
+      //generateToken
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie("x_auth", user.token).status(200).json({
+          loginSucess: true,
+        });
+      });
+    });
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({ success: true });
+  });
+});
+
+app.listen(5000);
